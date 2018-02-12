cb55d6266082e12940248b9d173f8ff10c8cf326
Bravo "Sasquatch" Tango <brian.tracy@btgraphix.com>
2018-02-12 11:31:13 -0800

    added HTML5 capture component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TakePhotoComponent } from './take-photo/take-photo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TakePhotoComponent
   ],
   imports: [
     BrowserModule
